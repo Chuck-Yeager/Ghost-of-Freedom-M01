b4d1f96beffade9957370574572cf6512201b687
Bobby Moretti <bobmoretti@gmail.com>
2022-07-24 00:04:38 -0700

    Initial version of respawn script. Will respan ~1 combined arms company when #T-72s drops below 31.

--- GoF_M01_1.0/l10n/DEFAULT/ctld_init.lua
+++ GoF_M01_1.0/l10n/DEFAULT/ctld_init.lua
@@ -54,3 +54,48 @@ for _, group in pairs(allBlueGroundGroups) do
         ctld.preLoadTransport(group:getName(), dismountComposition, true)
     end
 end
+
+local numCompaniesSpawned = 0
+local NUM_RESERVE_COMPANIES = 6
+local MIN_ALIVE_TANK_COUNT = 31
+
+local RESERVE_CO_TEMPLATES = {"Russian Reserve Tank CO-1", "Russian Reserve Tank CO-2", "Russian Reserve Tank CO-3",
+                              "Russian Reserve Mech Inf Platoon"}
+
+local reserveUnits = {}
+
+local function spawnNewCompany()
+    for _, templateName in ipairs(RESERVE_CO_TEMPLATES) do
+        local suffix = (numCompaniesSpawned + 1) .. "/" .. NUM_RESERVE_COMPANIES
+        local alias = templateName .. " " .. suffix
+        env.info("Spawning " .. alias)
+        local group = SPAWN:NewWithAlias(templateName, alias):Spawn()
+    end
+    numCompaniesSpawned = numCompaniesSpawned + 1
+end
+
+local function checkUnitsAlive()
+    if numCompaniesSpawned >= NUM_RESERVE_COMPANIES then
+        return
+    end
+
+    local tankCount = 0
+    local function incrementTankCountForGroup(group)
+        local numAliveUnits = group:CountAliveUnits()
+        local hasUnits = numAliveUnits > 0
+        if hasUnits and group:GetUnit(1):GetTypeName() == "T-72B" then
+            tankCount = tankCount + numAliveUnits
+        end
+    end
+
+    _DATABASE:ForEachGroup(incrementTankCountForGroup)
+    env.info("Found " .. tankCount .. " tanks alive")
+
+    if tankCount < MIN_ALIVE_TANK_COUNT then
+        env.info("Found fewer than " .. MIN_ALIVE_TANK_COUNT .. " tanks alive, spawning a new reserve company")
+        spawnNewCompany()
+    end
+end
+
+local unitAliverTimer = TIMER:New(checkUnitsAlive)
+unitAliverTimer:Start(nil, 60)
