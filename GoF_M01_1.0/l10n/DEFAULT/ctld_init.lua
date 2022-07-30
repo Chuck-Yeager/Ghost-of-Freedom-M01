7c3e55df0e9a19763360c740e3d8895ce2153ca8
Chuck <the.chuck.yeager@gmail.com>
2022-07-29 20:01:45 -0600

    preload troops according to unit name, not type

--- GoF_M01_1.0/l10n/DEFAULT/ctld_init.lua
+++ GoF_M01_1.0/l10n/DEFAULT/ctld_init.lua
@@ -1,59 +1,19 @@
-local PRELOAD_DISMOUNT_COMPOSITIONS = {
-    ["M-2 Bradley"] = {
-        mortar = 5,
-        at = 1,
-        mg = 1
-    },
-    ["M1126 Stryker ICV"] = {
-        inf = 2,
-        mg = 1,
-        at = 5,
-        aa = 1
-    }
+local INFANTRY_SQUAD = {        
+    inf = 2,
+    mg = 1,
+    at = 4
 }
 
-local function contains(tab, val)
-    for index, value in ipairs(tab) do
-        if value == val then
-            return true
-        end
-    end
-    return false
-end
+local MORTAR_SQUAD = {
+    mg =1,
+    aa = 1,
+    mortar = 5
+}
 
-local function hasKey(tab, idx)
-    return tab[idx] ~= nil
-end
+ctld.preLoadTransport("1-1/A", INFANTRY_SQUAD, true)
+ctld.preLoadTransport("6-6/A", INFANTRY_SQUAD, true)
 
-local function groupHasDismounts(groupName)
-    -- make sure the group name follows the pattern n-m/Z
-    -- where n and m are numbers and Z is a letter.
-    local hasDash = groupName:sub(2, 2) == "-"
-    local hasSlash = groupName:sub(4, 4) == "/"
-    local platoonIndex = groupName:sub(1, 1)
-    local companyName = groupName:sub(5, 5)
-    local inTransportGroup = contains(ctld.transportPilotNames, groupName)
-    return hasDash and hasSlash and inTransportGroup
-end
 
-local function shouldPreload(group)
-    local unit = group:getUnit(1)
-    unitType = unit:getTypeName()
-    local isAppropriateVehicle = hasKey(PRELOAD_DISMOUNT_COMPOSITIONS, unitType)
-    local isAppropriateGroup = groupHasDismounts(group:getName())
-    return isAppropriateVehicle and isAppropriateGroup
-end
-
-local allBlueGroundGroups = coalition.getGroups(2, Group.Category.GROUND)
-
-for _, group in pairs(allBlueGroundGroups) do
-    if shouldPreload(group) then
-        env.info("Preloading " .. group:getName())
-        local unitType = group:getUnit(1):getTypeName()
-        local dismountComposition = PRELOAD_DISMOUNT_COMPOSITIONS[unitType]
-        ctld.preLoadTransport(group:getName(), dismountComposition, true)
-    end
-end
 
 local numCompaniesSpawned = 0
 local NUM_RESERVE_COMPANIES = 6
